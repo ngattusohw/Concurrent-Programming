71521bc8a179b59095b1d29757a0903c9b945c94
ngattusohw <ngattusohw@gmail.com>
2018-09-03 18:47:01 -0400

    Adding working single thread implementation for assignmentone, need to figure out list of threads and also need to handle special cases as stated in the homework

--- AssignmentOne/src/AssignmentOne.java
+++ AssignmentOne/src/AssignmentOne.java
@@ -8,11 +8,25 @@ public class AssignmentOne extends PrimeFinder{
 	}
 	
 	
-	
-	//List<Integer> intervals
+	//List<Integer[]> intervals
 	public static List<Integer> lprimes(){
-		PrimeFinder test = new PrimeFinder(1,2);
-		List<Integer> testicle = test.getPrimesList();
+		PrimeFinder ass = new PrimeFinder(2,10);
+		Thread thread1 = new Thread(ass);
+		thread1.start();
+		try {
+			thread1.join();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		List<Integer> fuckingdicks = ass.getPrimesList();
+		System.out.println("Eat my ass");
+		for(Integer hold : fuckingdicks) {
+			System.out.println("This is the shit:: " + hold);
+		}
+		
+//		PrimeFinder test = new PrimeFinder(1,2);
+//		List<Integer> testicle = test.getPrimesList();
 		return null;
 	}
 	
--- AssignmentOne/src/PrimeFinder.java
+++ AssignmentOne/src/PrimeFinder.java
@@ -1,41 +1,52 @@
+import java.util.ArrayList;
 import java.util.List;
 
 public class PrimeFinder implements Runnable{
 
 	private Integer start ;
 	private Integer end ;
-	private List<Integer> primes ;
+	private List<Integer> primes;
 	
 	// Constructs a PrimeFinder
 	public PrimeFinder(Integer startNum , Integer endNum) {
 		start = startNum;
 		end = endNum;
+		primes = new ArrayList<Integer>();
 	}
 	
 	// Returns the value of the attribute primes
 	public List<Integer> getPrimesList(){
-		
-		Runnable runnable = new PrimeFinder(start,end);
-		Thread thread = new Thread(runnable);
-		thread.start();
-		
-		
-		return null;
+		return primes;
 	}
 
 	// Determines whether its argument is prime or not
 	public Boolean isPrime(int n){
-		return false;
+	    if (n%2==0) {
+	    	return false;
+	    }
+	    for(int i=3;(i*i)<=n;i+=2) {
+	        if(n%i==0) {
+	            return false;
+	        }
+	    }
+	    return true;
 	}
 	
 	// Adds all primes in [ this . start , this . end ) to the attribute primes
 	public void run () {
 		System.out.println("This is me, I am in the thread!");
-		//primes = getPrimesList();
-		
+		for(int s=start;s<=end;s++) {
+			if(isPrime(s)) {
+				primes.add(s);
+			}
+		}
 	}
 	
 //	public static void main(String args[]) {
 //		
 //	}
+	
+//	Runnable runnable = new PrimeFinder(start,end);
+//	Thread thread = new Thread(runnable);
+//	thread.start();
 }
